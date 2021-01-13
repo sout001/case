9384f01720ae47d183b50dfc8b30ccd80e377cd5
chenshaoqiang <chenshaoqiang@huanyin.onaliyun.com>
2021-01-13 10:58:14 +0800

    lasted case commit

--- case/urls.py
+++ case/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from case.views import index_view, play
+from case.views import index_view, play, download
 
 urlpatterns = [
     path('index', index_view, name='index'),
-    path('play', play, name='play')
+    path('play', play, name='play'),
+    path('download', download, name='download')
 ]
--- case/views.py
+++ case/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import FileResponse
+import requests
+from urllib import parse
 
 
 # Create your views here.
@@ -8,3 +12,11 @@ def index_view(request):
 
 def play(request):
     return render(request, 'index_form.html')
+
+
+def download(request):
+    rs = requests.get('https://app.onenine.cc/m/api/url/yqd/id/6794OCXgTdmHjbmQtlqWUR1UqxO-repLm79t8g_uP3DTr0CLniTW/format/128000')
+    response = FileResponse(rs, content_type='1.png')
+    response['Content-Type'] = 'application/octet-stream'
+    response['Content-Disposition'] = 'attachment;filename=' + parse.quote('1.mp3', encoding="utf-8")
+    return response
